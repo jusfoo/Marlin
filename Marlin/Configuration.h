504dceae0ed9993461893d6056f3f74856800cdf
Dan Royer <dan@marginallyclever.com>
2022-01-27 11:34:46 -0800

    added some helpful comments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1373,16 +1373,16 @@
 
 // @section machine
 
-// The size of the printable area
+// The size of the printable area.  Must be a whole number.  Cannot be an odd number.
 #define X_BED_SIZE 650
 #define Y_BED_SIZE 1000
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -325
-#define Y_MIN_POS -500
+#define X_MIN_POS (-X_BED_SIZE/2)
+#define Y_MIN_POS (-Y_BED_SIZE/2)
 #define Z_MIN_POS 0
-#define X_MAX_POS 325
-#define Y_MAX_POS 500
+#define X_MAX_POS (X_BED_SIZE/2)
+#define Y_MAX_POS (Y_BED_SIZE/2)
 #define Z_MAX_POS 200
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
@@ -1728,7 +1728,9 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
+// MANUAL_X_HOME_POS must be within the range X_MAX_POS...X_MIN_POS.
 #define MANUAL_X_HOME_POS 0
+// MANUAL_Y_HOME_POS must be within the range Y_MAX_POS...Y_MIN_POS.
 #define MANUAL_Y_HOME_POS -482.65 //(Y_MAX_POS-( sqrt(sq(POLARGRAPH_MAX_BELT_LEN)-sq(X_BED_SIZE/2))))
 //#define MANUAL_Z_HOME_POS 0
 //#define MANUAL_I_HOME_POS 0
